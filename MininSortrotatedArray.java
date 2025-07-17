9c181a79dde5d2d5d28e8ebec289b8f17ed06f92
Urvashi Agrawal <158180920+urvashi-agrawal-dev@users.noreply.github.com>
2025-07-17 06:47:29 +0000

    v

--- MininSortrotatedArray.java
+++ MininSortrotatedArray.java
@@ -1,3 +1,28 @@
+//Min in Sort rotated Array this is the first approach below is the second approach
+import java.util.*;
+class MininSortrotatedArray{
+    static int msra(int[] arr){
+        int n=arr.length;
+        int low=0;int high=n-1;
+        while (low<high){
+            int mid=(low+high)/2;
+            if(arr[mid]>arr[high]){
+                low=mid+1;
+            } else{
+                high=mid;
+            }
+        }
+        return arr[low];
+    }
+    public static void main(String[] args){
+        int[] arr={9,4,5,6,11};
+        System.out.println(msra(arr));
+    }
+}
+
+
+//this is the second approach more optimal
+    
 import java.util.*;
 class MininSortrotatedArray{
     static int msra(int[] arr){
